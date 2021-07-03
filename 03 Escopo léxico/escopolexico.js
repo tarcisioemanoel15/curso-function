3bb15be351c5024048d2ab14bae75f39b0d20446
tarcisioemanoel15 <caninosbrancos10031@gmail.com>
2021-07-03 14:41:43 -0300

    escopoFuncao

---
+++ 03 Escopo léxico/escopolexico.js
@@ -0,0 +1,15 @@
+const nome = "Tarcisio";
+
+function falanome(){
+    const nome = "Dos";
+    console.log(nome);
+}
+
+
+
+function ufalanome(){
+    falanome();
+}
+ufalanome();
+falanome();
+    
