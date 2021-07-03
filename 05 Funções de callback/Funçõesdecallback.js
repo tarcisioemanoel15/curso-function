8227a9b8a01a9e259c40bdfa830dc0eccc1418e5
tarcisioemanoel15 <caninosbrancos10031@gmail.com>
2021-07-03 17:50:03 -0300

    calback

---
+++ 05 Funções de callback/Funçõesdecallback.js
@@ -0,0 +1,66 @@
+//função que gera os numeros aleatorios
+
+function rand(min = 1000, max = 300){
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num);
+}
+
+console.log(rand());
+
+
+
+function f1(callback){
+
+     setTimeout(function(){
+        console.log("f1");
+        if(callback) callback();
+    }, rand());
+
+}
+
+function f2(callback){
+
+    setTimeout(function(){
+        console.log("f2");
+        if(callback) callback();
+    }, rand());
+
+}
+
+function f3(callback){
+
+    setTimeout(function(){
+        console.log("f3");
+        if(callback) callback();
+    }, rand());
+
+}
+
+/*
+f1(function(){
+    f2(function(){
+        f3(function(){
+            console.log("ola Mundo");
+        });
+    });
+});
+*/
+
+f1(f1callback);
+
+function f1callback(){
+    f2(f2callback);
+}
+
+function f2callback(){
+    f3(f3callback);
+}
+
+
+function f3callback(){
+    console.log("ola Mundo");
+}
+    
+
+
+
