729f1318b58788899e32479f933a34972c248841
tarcisioemanoel15 <caninosbrancos10031@gmail.com>
2021-07-09 23:40:12 -0300

    comit

--- 76543210estudarExeAnterior/teste.js
+++ 76543210estudarExeAnterior/teste.js
@@ -27,13 +27,10 @@ function Callbacksoma(){
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 
-
-function MediaDeAlunos(){
-    //Media de alunos sinples
-const a = 7;
-const b = 7;
-const c = 7;
-const d = 7;
+//      função auto invocado
+/* (function(a, s, a){ ****})(); */
+ 
+(function(a = 7, b = 7, c = 7, d = 7){  
 
 function somaMrdia(a, b, c, d){
     return a + b + c + d;
@@ -51,7 +48,101 @@ function calculaMedia(nota){
 
 console.log(calculaMedia(nota));
 
+
+})();
+
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+// Funcao fabrica sinples
+
+function fabrica(){
+    return {
+        nome: "Tarcisio",
+        sobrenome: "emanuel",
+        idade: 30,
+        peso: 60,
+        altura: 1.60,
+        
+        imc () {
+            const indice = this.peso / (this.altura **2);
+            return `Seu indice de massa corporal é : ${indice.toFixed(2)}`;
+        },
+        
+        fala: function(){
+            return `${this.nome} diz OOOOOOOOOIIIIIIII`;
+        }
+        
+    }
 }
-MediaDeAlunos();
+
+const p1 = fabrica();
+
+// console.log(p1.imc());
+// console.log(p1.fala());
+
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+/////////Testes funcoes de fabrica e getrer e seter  ///////
+
+
+function tab(a, b){
+    return {
+        get soma(){
+            const som = a + b;
+            return som;
+        },
+        
+        sub() {
+        const y = a - b;
+        return y;
+        },
+        
+         mul: function(){
+        const multiplica = a * b;
+        return multiplica;
+    },
+    
+        divi: function(){
+        const dividir = a / b;
+        return dividir;
+        }
+    }
+}
+        
+
+/**/
+
+const tabuada = tab(5, 6);
+
+//console.log(tabuada.soma);
+//console.log(tabuada.sub());
+//console.log(tabuada.mul());
+
+var _v = 0;
+
+var oobj = { 
+    get v (){
+        return 'value' +  _v;
+    },
+
+    set v(value){
+        return  _v = value + 2;
+    }
+}
+    
+
+
+//console.log(oobj.v = 7);
+console.log(oobj._v = 7);
 
 
